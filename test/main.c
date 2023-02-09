b9467f442f32a75da2dc2186c3bab8d0d9a0484e
lqm283 <lanqianming@hotmail.com>
2023-02-09 11:53:29 +0800

    编写了最简单的cmake的文档

---
+++ test/main.c
@@ -0,0 +1,16 @@
+/*
+ * @Author       : lqm283
+ * @Date         : 2023-02-09 11:40:14
+ * @LastEditTime : 2023-02-09 11:45:04
+ * @LastEditors  : lqm283
+ * --------------------------------------------------------------------------------<
+ * @Description  : Please edit a descrition about this file at here.
+ * --------------------------------------------------------------------------------<
+ * @FilePath     : /cmake/test/main.c
+ */
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("hello world\r\n");
+    return 0;
+}
