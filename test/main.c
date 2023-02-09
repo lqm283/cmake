fabcec67297f310008d6e45cee37d3e9d7083cc9
lqm283 <lanqianming@hotmail.com>
2023-02-09 15:54:02 +0800

    编写了设置工程版本的文档

--- test/main.c
+++ test/main.c
@@ -1,7 +1,7 @@
 /*
  * @Author       : lqm283
  * @Date         : 2023-02-09 11:40:14
- * @LastEditTime : 2023-02-09 14:59:47
+ * @LastEditTime : 2023-02-09 15:17:42
  * @LastEditors  : lqm283
  * --------------------------------------------------------------------------------<
  * @Description  : Please edit a descrition about this file at here.
@@ -10,7 +10,10 @@
  */
 #include <stdio.h>
 
+#include "config.h"
+
 int main(int argc, char **argv) {
     printf("hello world\r\n");
+    printf("project version is v%d.%d \n", main_VERSION_MAJOR, main_VERSION_MINOR);
     return 0;
 }
